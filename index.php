6e678212effa47dc237542cc2a832034f48b45cf
Chakrit Tansakul <mynamecoad@gmail.com>
2016-04-28 11:14:26 +0700

    -add flatui, init index page

--- index.php
+++ index.php
@@ -1,3 +1,66 @@
-<?php
-echo "test";
-?>
+<php?
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Smart Management</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="flatui/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Loading Flat UI -->
+    <link href="flatui/css/flat-ui.min.css" rel="stylesheet">
+
+    <link rel="shortcut icon" href="flatui/img/favicon.ico">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+      <script src="flatui/js/vendor/html5shiv.js"></script>
+      <script src="flatui/js/vendor/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
+    <script src="flatui/js/vendor/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="flatui/js/vendor/video.js"></script>
+    <script src="flatui/js/flat-ui.min.js"></script>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Hello, there!</h1>
+                <p>Welcome to Smart Management</p>
+            </div>
+        </div>
+        <div class="row">
+            <hr/>
+        </div>
+        <div class="row" align="center">
+            <div class="col-xs-12">
+                <h3>Select your menu</h3>
+            </div>
+        </div>
+        <div class="row" align="center">
+            <div class="col-xs-6">
+                <span class="fui-new" style="font-size:180px;"></span>
+                <p>Add promotion</p>
+            </div>
+            <div class="col-xs-6">
+                <span class="fui-window" style="font-size:180px;"></span>
+                <p>View statistics</p>
+            </div>
+        </div>
+        <br/>
+        <br/>
+        <br/>
+        <div class="row">
+            <hr/>
+        </div>
+        <div class="row" align="center">
+            <p style="font-size:16px">Powered by Smart Management Team</p>
+        </div>
+    </div>
+    <!-- /.container -->
+  </body>
+</html>
