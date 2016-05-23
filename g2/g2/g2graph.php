911cdbe2e1baf5dc9562470ebf619a7e20205127
Chakrit Tansakul <mynamecoad@gmail.com>
2016-05-23 15:49:25 +0700

    fixed date bug in g2 graph

--- g2/g2/g2graph.php
+++ g2/g2/g2graph.php
@@ -9,18 +9,18 @@ $sql = "SELECT DAY(ts) as day, COUNT(*) as entry FROM entry WHERE ts BETWEEN '".
 $result = $conn->query($sql) or trigger_error($conn->error);
 $sum_entry = array();
 while ($row = $result->fetch_array()){
-  $sum_entry[$row['day']] = $row['entry'];
+  $sum_entry[$row['day']-1] = $row['entry'];
 }
 
 $sum_entry_all = array();
 $string_return = "";
-for($i=0;$i<=31;$i++){
+for($i=0;$i<31;$i++){
 	if(!isset($sum_entry[$i])){
 		$sum_entry_all[$i] = 0;
 	}else{
 		$sum_entry_all[$i] = (int)$sum_entry[$i];
 	}
-	if($i!=31){
+	if($i!=30){
 		$string_return = $string_return . $sum_entry_all[$i] . ',';
 	}else{
 		$string_return = $string_return . $sum_entry_all[$i];
